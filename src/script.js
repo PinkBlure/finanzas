f550485cae3d4355436e4551418a70da8dba3ef2
aileen <aileenpadrontorres511@gmail.com>
2024-12-01 22:02:18 +0000

    first commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+function loginUser(username, password) {
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    window.location.href = './src/pages/home.html';
+  } else {
+    alert('Usuario o contraseña incorrectos.');
+  }
+}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username && password) {
+    loginUser(username, password);
+  } else {
+    alert('Por favor ingresa tu nombre de usuario y contraseña.');
+  }
+});
